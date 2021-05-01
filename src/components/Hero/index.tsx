3d395f0de1f592a13f4ca5d82f257012ea54d367
Fábio Alves <fabioalves.contacto@gmail.com>
2021-05-01 13:52:01 -0100

    feat: hero done

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
 import styles from "./hero.module.scss";
 
 export function Hero() {
-  return <div></div>;
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroInfo}>
+        <p>Hello, i'm </p>
+        <h1>Fábio Alves</h1>
+        <p>Front-End Developer and Mobile Developer.</p>
+      </div>
+
+      <div className={styles.heroImage}>
+        <Image src="/fsk.jpg" width={500} height={500} />
+      </div>
+    </div>
+  );
 }
